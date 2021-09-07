22913ed3b8bcfb805f8be4e82bbe01c26bdfc703
Mariia Berger <slackerpanda@gmail.com>
2021-09-07 17:15:44 +0300

    Initial Commit

---
+++ streetlights 3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  streetlights 3.1
+//
+//  Created by Maria Berger on 07.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ streetlights 3.1/streetlights_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  streetlights_3_1App.swift
+//  streetlights 3.1
+//
+//  Created by Maria Berger on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct streetlights_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
