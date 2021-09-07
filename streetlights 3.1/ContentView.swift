f136417ea4ef3de9079a87faf8763f7ccb7298ea
Mariia Berger <slackerpanda@gmail.com>
2021-09-07 20:56:34 +0300

    set up the mainView

--- streetlights 3.1/ContentView.swift
+++ streetlights 3.1/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    var redCircle = Circle()
+    var yellowCircle = Circle()
+    var greenCircle = Circle()
+    @State var buttonText = "Start"
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Circles
+            Button(action: switchColors, label: {
+                Text(buttonText)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 50, alignment: .center)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            })
+        }
+        .ignoresSafeArea()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black)
+        
+    }
+    
+    
+    private var Circles: some View {
+        VStack(alignment: .center){
+                redCircle
+                .foregroundColor(.red)
+                .background(Color.black)
+                .frame(width: 100, height: 100, alignment: .center)
+                .overlay(Circle().stroke(Color.white, lineWidth: 3.0))
+                    .opacity(0.3)
+            
+                yellowCircle
+                .foregroundColor(.yellow)
+                .background(Color.black)
+                .frame(width: 100, height: 100, alignment: .center)
+                .overlay(Circle().stroke(Color.white, lineWidth: 3.0))
+                    .opacity(0.3)
+                greenCircle
+                .foregroundColor(.green)
+                .background(Color.black)
+                .frame(width: 100, height: 100, alignment: .center)
+                .overlay(Circle().stroke(Color.white, lineWidth: 3.0))
+                    .opacity(0.3)
+        }
+        .padding(.top, -160.0)
+        .ignoresSafeArea()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+
+
+    func switchColors() {
+//        if yellowCircle.opacity(0.3) || redCircle.opacity(1.0) {
+//            buttonText = "NEXT"
+//            yellowCircle.opacity(1.0)
+//            redCircle.opacity(0.3)}
+//        if greenCircle.opacity(0.3) as! Bool || yellowCircle.opacity(1.0) as! Bool {
+//            buttonText = "NEXT"
+//            greenCircle.opacity(1.0)
+//            yellowCircle.opacity(0.3)}
+//        else {
+//            buttonText = "NEXT"
+//            redCircle.opacity(1.0)
+//            greenCircle.opacity(0.3)
+//        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
